cfa7a38cf5e2aa20ee3d2d37442c5fd03d2761f0
dhruti21 <desaidhruti.21@gmail.com>
2019-02-20 21:13:53 -0700

    Adding 3D model in IOS

---
+++ IHO/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  IHO
+//
+//  Created by Desai, Dhruti on 2/19/19.
+//  Copyright © 2019 Desai, Dhruti. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+}
