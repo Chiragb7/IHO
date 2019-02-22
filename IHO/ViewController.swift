8278857077d115482d885d97e1cd44b23b91b0a3
Vineesha Kasam <vineesha1416.cse@gmail.com>
2019-02-22 16:54:29 -0700

    Added a Label and a Button while setting up their properties at the top of the IHO AR app

--- IHO/ViewController.swift
+++ IHO/ViewController.swift
@@ -23,6 +23,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        // create a new label on the ARSCNView specific to the 3D model object
+        let sceneARLabel = UILabel(frame: CGRect(x: 20, y: 20, width: 500, height: 40))
+        
+        // setting the different properties of the sceneARLabel
+        // set the respective text
+        sceneARLabel.text = "Air Plane Label"
+        // set the alignment of the label
+        sceneARLabel.textAlignment = .left
+        // set the font size of the label
+        sceneARLabel.font = sceneARLabel.font.withSize(20)
+        // set the colour of the label text
+        sceneARLabel.textColor = UIColor.white
+        
+        // adding the Label to the view
+        sceneView.addSubview(sceneARLabel)
+        
+        
+        let sceneARButton = UIButton(frame: CGRect(x: 50, y: 50, width: 300, height: 50))
+        
+        // setting the attributes for the sceneARButton
+        // set the title for the button
+        sceneARButton.setTitle("Touch Model Button", for: .normal)
+        // set the color of the title
+        sceneARButton.setTitleColor(UIColor.white, for: .normal)
+    
+        //adding the button to the view
+        sceneView.addSubview(sceneARButton)
+        
         // Create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
