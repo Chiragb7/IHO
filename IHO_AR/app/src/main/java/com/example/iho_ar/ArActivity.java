d4369f9404cd391ac19d44779052774230fce2f8
Chiragb7 <cbumb@asu.edu>
2019-02-25 00:00:12 -0700

    Task #40

--- IHO_AR/app/src/main/java/com/example/iho_ar/ArActivity.java
+++ IHO_AR/app/src/main/java/com/example/iho_ar/ArActivity.java
@@ -4,15 +4,29 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
 
 public class ArActivity extends AppCompatActivity {
     private static final String TAG = ArActivity.class.getSimpleName();
     private static final double MIN_OPENGL_VERSION = 3.0;
+    private ArFragment arFragment;
+    private ModelRenderable skullRenderable;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +35,41 @@ public class ArActivity extends AppCompatActivity {
             return;
         }
 
+
+
         setContentView(R.layout.activity_main);
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.AR_fragment);
+
+        ModelRenderable.builder()
+                .setSource(this, R.raw.skull)
+                .build()
+                .thenAccept(renderable -> skullRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast toast =
+                                    Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                            return null;
+                        });
+
+
+        arFragment.setOnTapArPlaneListener(
+                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+                    if (skullRenderable == null) {
+                        return;
+                    }
+
+                    // Create the Anchor.
+                    Anchor anchor = hitResult.createAnchor();
+                    AnchorNode anchorNode = new AnchorNode(anchor);
+                    anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                    // Create the transformable andy and add it to the anchor.
+                    Node skull = new Node();
+                    skull.setParent(anchorNode);
+                    skull.setRenderable(skullRenderable);
+                });
     }
 
     public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
