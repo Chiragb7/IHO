9c065f0ee78dabd7f477e7152ec94ec12f0cc17f
divya-yadamreddi <dyadamre@asu.edu>
2019-03-22 18:42:06 -0700

    US#51 Task#56 Added lucy to scenekit

--- README.md
+++
@@ -1,2 +0,0 @@
-# IHO
-Capstone Project - Institute of Human Origins
---
+++ iOS/IHO/IHO/ViewController.swift
@@ -0,0 +1,118 @@
+//
+//  ViewController.swift
+//  IHO
+//
+//  Created by vkasam on 3/22/19.
+//  Copyright © 2019 vkasam. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet var infoLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/Lucy.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    func updatePositionAndOrientationOf(_ node: SCNNode, withPosition position: SCNVector3, relativeTo referenceNode: SCNNode) {
+        let referenceNodeTransform = matrix_float4x4(referenceNode.transform)
+        
+        // Setup a translation matrix with the desired position
+        var translationMatrix = matrix_identity_float4x4
+        translationMatrix.columns.3.x = position.x
+        translationMatrix.columns.3.y = position.y
+        translationMatrix.columns.3.z = position.z
+        
+        // Combine the configured translation matrix with the referenceNode's transform to get the desired position AND orientation
+        let updatedTransform = matrix_multiply(referenceNodeTransform, translationMatrix)
+        node.transform = SCNMatrix4(updatedTransform)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+ */
+    
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera:ARCamera, didFailWithError error: Error) {
+        switch camera.trackingState {
+        case .normal :
+            infoLabel.text = "Move the device to detect horizontal surfaces."
+            
+        case .notAvailable:
+            infoLabel.text = "Tracking not available."
+            
+        case .limited(.excessiveMotion):
+            infoLabel.text = "Tracking limited - Move the device more slowly."
+            
+        case .limited(.insufficientFeatures):
+            infoLabel.text = "Tracking limited - Point the device at an area with visible surface detail."
+            
+        case .limited(.initializing):
+            infoLabel.text = "Initializing AR session."
+            
+        default:
+            infoLabel.text = ""
+        }
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        infoLabel.text = "Session was interrupted"
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        infoLabel.text = "Session interruption ended"
+        resetTracking()
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+    
+    func resetTracking() {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+}
