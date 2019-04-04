4f6b613d47ea1088f3775057704eeeffd558c53d
Chiragb7 <cbumb@asu.edu>
2019-04-03 21:55:03 -0700

    Task #63

--- Android/IHO_AR/app/src/main/java/com/iho/iho_ar/IhoApplication.java
+++ Android/IHO_AR/app/src/main/java/com/iho/iho_ar/IhoApplication.java
@@ -6,6 +6,7 @@ import com.google.ar.sceneform.math.Vector3;
 import com.iho.iho_ar.models.BoneModel;
 import com.iho.iho_ar.models.BoneModelList;
 
+
 public class IhoApplication extends Application {
     @Override
     public void onCreate() {
@@ -17,17 +18,17 @@ public class IhoApplication extends Application {
         Vector3 buttonScale = new Vector3(0.1f,0.1f,0.1f);
         BoneModelList.getInstance().addToBoneModelList(new BoneModel("Lucy","",R.raw.lucy,
                 new Vector3(0,0,0), false));
-        BoneModelList.getInstance().addToBoneModelList(new BoneModel("Hand","Right Hand",R.raw.button2,
+        BoneModelList.getInstance().addToBoneModelList(new BoneModel("Hand",getResources().getString(R.string.right_hand),R.raw.button2,
                 new Vector3(-0.9f,1.3f,0), buttonScale));
-        BoneModelList.getInstance().addToBoneModelList(new BoneModel("Foot","Left Foot",
+        BoneModelList.getInstance().addToBoneModelList(new BoneModel("Foot",getResources().getString(R.string.left_foot),
                 R.raw.button2, new Vector3(0.345f,0,0), buttonScale));
-        BoneModelList.getInstance().addToBoneModelList(new BoneModel("Knee","Right Knee",
+        BoneModelList.getInstance().addToBoneModelList(new BoneModel("Knee",getResources().getString(R.string.right_knee),
                 R.raw.button2, new Vector3(-0.2f,0.4f,0), buttonScale));
         BoneModelList.getInstance().addToBoneModelList(new BoneModel("Arm","Right Arm",
                 R.raw.button2, new Vector3(-0.3f,1.3f,0), buttonScale));
         BoneModelList.getInstance().addToBoneModelList(new BoneModel("Arm","Left Arm",
                 R.raw.button2, new Vector3(0.275f,1.3f,0), buttonScale));
-        BoneModelList.getInstance().addToBoneModelList(new BoneModel("Thigh","Left Thigh",
+        BoneModelList.getInstance().addToBoneModelList(new BoneModel("Thigh",getResources().getString(R.string.left_thigh),
                 R.raw.button2, new Vector3(0.2f,0.6f,0), buttonScale));
         BoneModelList.getInstance().addToBoneModelList(new BoneModel("Spine","Spine",
                 R.raw.button2, new Vector3(0f,1f,-0.11f), buttonScale));
