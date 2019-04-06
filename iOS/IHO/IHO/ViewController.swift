1f12de40825e5a9b1f568efe42c8029a57223af8
divya-yadamreddi <dyadamre@asu.edu>
2019-04-05 17:22:12 -0700

    Added an alert to display the text as an alert instead of an 3D text pop

--- iOS/IHO/IHO/ViewController.swift
+++ iOS/IHO/IHO/ViewController.swift
@@ -44,6 +44,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let LucySceneChildNodes = LucyScene.rootNode.childNodes
         
         for childNode in LucySceneChildNodes {
+            childNode.name = "rightLeg"
             LucyNode.addChildNode(childNode)
         }
         LucyNode.position = SCNVector3(x, y, z)
@@ -57,6 +58,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let LucySceneChildNodes = LucyScene.rootNode.childNodes
         
         for childNode in LucySceneChildNodes {
+            childNode.name = "rightFeet"
             LucyNode.addChildNode(childNode)
         }
         LucyNode.position = SCNVector3(x, y, z)
@@ -65,22 +67,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func addButtonPalm(x: Float = 0.099, y: Float = 0.08, z: Float = 0) {
-    guard let LucyScene = SCNScene(named: "art.scnassets/button/button.scn") else { return }
-    let LucyNodeH = SCNNode()
-    let LucySceneChildNodes = LucyScene.rootNode.childNodes
-    for childNode in LucySceneChildNodes {
-    LucyNodeH.addChildNode(childNode)
-    }
-    LucyNodeH.position = SCNVector3(x, y, z)
-    LucyNodeH.scale = SCNVector3(0.01, 0.01, 0.01)
-    sceneView.scene.rootNode.addChildNode(LucyNodeH)
+        guard let LucyScene = SCNScene(named: "art.scnassets/button/button.scn") else { return }
+        let LucyNodeH = SCNNode()
+        let LucySceneChildNodes = LucyScene.rootNode.childNodes
+        
+        for childNode in LucySceneChildNodes {
+            childNode.name = "leftFingers"
+            LucyNodeH.addChildNode(childNode)
+        }
+        LucyNodeH.position = SCNVector3(x, y, z)
+        LucyNodeH.scale = SCNVector3(0.01, 0.01, 0.01)
+        sceneView.scene.rootNode.addChildNode(LucyNodeH)
     }
     
     func addButtonHand(x: Float = -0.06, y: Float = 0.08, z: Float = 0) {
         guard let LucyScene = SCNScene(named: "art.scnassets/button/button.scn") else { return }
         let LucyNodeH = SCNNode()
         let LucySceneChildNodes = LucyScene.rootNode.childNodes
+        
         for childNode in LucySceneChildNodes {
+            childNode.name = "leftHand"
             LucyNodeH.addChildNode(childNode)
         }
         LucyNodeH.position = SCNVector3(x, y, z)
@@ -92,7 +98,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         guard let LucyScene = SCNScene(named: "art.scnassets/button/button.scn") else { return }
         let LucyNodeH = SCNNode()
         let LucySceneChildNodes = LucyScene.rootNode.childNodes
+        
         for childNode in LucySceneChildNodes {
+            childNode.name = "rightHip"
             LucyNodeH.addChildNode(childNode)
         }
         LucyNodeH.position = SCNVector3(x, y, z)
@@ -104,7 +112,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         guard let LucyScene = SCNScene(named: "art.scnassets/button/button.scn") else { return }
         let LucyNodeH = SCNNode()
         let LucySceneChildNodes = LucyScene.rootNode.childNodes
+        
         for childNode in LucySceneChildNodes {
+            childNode.name = "spine"
             LucyNodeH.addChildNode(childNode)
         }
         LucyNodeH.position = SCNVector3(x, y, z)
@@ -116,7 +126,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         guard let LucyScene = SCNScene(named: "art.scnassets/button/button.scn") else { return }
         let LucyNodeH = SCNNode()
         let LucySceneChildNodes = LucyScene.rootNode.childNodes
+        
         for childNode in LucySceneChildNodes {
+            childNode.name = "leftLegBone"
             LucyNodeH.addChildNode(childNode)
         }
         LucyNodeH.position = SCNVector3(x, y, z)
@@ -127,9 +139,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func addButtonLHand(x: Float = -0.2, y: Float = 0.085, z: Float = 0) {
         guard let LucyScene = SCNScene(named: "art.scnassets/button/button.scn") else { return }
         let LucyNodeH = SCNNode()
-        LucyNodeH.name = "Sphere1"
         let LucySceneChildNodes = LucyScene.rootNode.childNodes
+        
         for childNode in LucySceneChildNodes {
+            childNode.name = "rightHand"
             LucyNodeH.addChildNode(childNode)
         }
         LucyNodeH.position = SCNVector3(x, y, z)
@@ -141,7 +154,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         guard let LucyScene = SCNScene(named: "art.scnassets/button/button.scn") else { return }
         let LucyNodeH = SCNNode()
         let LucySceneChildNodes = LucyScene.rootNode.childNodes
+        
         for childNode in LucySceneChildNodes {
+            childNode.name = "lowerJaw"
             LucyNodeH.addChildNode(childNode)
         }
         LucyNodeH.position = SCNVector3(x, y, z)
@@ -153,7 +168,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         guard let LucyScene = SCNScene(named: "art.scnassets/button/button.scn") else { return }
         let LucyNodeH = SCNNode()
         let LucySceneChildNodes = LucyScene.rootNode.childNodes
+        
         for childNode in LucySceneChildNodes {
+            childNode.name = "skull"
             LucyNodeH.addChildNode(childNode)
         }
         LucyNodeH.position = SCNVector3(x, y, z)
@@ -178,14 +195,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         addButtonLHand()
         addButtonLSkull()
         addButtonHSkull()
-
-        //Adding Text pop up
-        let scaleGesture = UIPinchGestureRecognizer(target: self, action: #selector(scaleCurrentNode(_:)))
-        self.view.addGestureRecognizer(scaleGesture)
-        
-        //3. Add A Tap Gesture Recogizer So We Can Place Our TextNode
-       // let tapGesture = UITapGestureRecognizer(target: self, action: #selector(placeOrAssignNode(_:)))
-        //self.view.addGestureRecognizer(tapGesture)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
@@ -197,37 +206,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 return
         }
         print(nodeName)
+        var msg = ""
+        //Adding alert to display the text.
+        let alert = UIAlertController(title: "", message: msg, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true)
         
-        if let hitTest = self.sceneView.hitTest(touchLocation, types: .featurePoint).first{
-            
-            //4. Get The World Transform
-            let hitTestPosition = hitTest.worldTransform.columns.3
-            
-            //5. Add The TestNode At The Desired Position
-            if nodeName == "Sphere"{
-                let string = "Sphere 1"
-                createTextFromPosition(SCNVector3(hitTestPosition.x, hitTestPosition.y, hitTestPosition.z), string)
-                return
-            }
-            else{
-                return
-            }
-        }
-        currentNode?.removeFromParentNode()
-        
-    }
-    func updatePositionAndOrientationOf(_ node: SCNNode, withPosition position: SCNVector3, relativeTo referenceNode: SCNNode) {
-        let referenceNodeTransform = matrix_float4x4(referenceNode.transform)
-        
-        // Setup a translation matrix with the desired position
-        var translationMatrix = matrix_identity_float4x4
-        translationMatrix.columns.3.x = position.x
-        translationMatrix.columns.3.y = position.y
-        translationMatrix.columns.3.z = position.z
-        
-        // Combine the configured translation matrix with the referenceNode's transform to get the desired position AND orientation
-        let updatedTransform = matrix_multiply(referenceNodeTransform, translationMatrix)
-        node.transform = SCNMatrix4(updatedTransform)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -246,17 +230,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
- */
     
     func session(_ session: ARSession, cameraDidChangeTrackingState camera:ARCamera, didFailWithError error: Error) {
         switch camera.trackingState {
@@ -305,100 +278,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.didReceiveMemoryWarning()
         
     }
-    
-    //-----------------------
-    //MARK: Touch Interaction
-    //-----------------------
-    
-    /// Performs An ARHitTest Or SCNHitTest So We Can Place Or Assign Our TextNode
-    ///
-    /// - Parameter gesture: UITapGestureRecognizer
- /*   @objc func placeOrAssignNode(_ gesture: UITapGestureRecognizer){
-        
-        //1. Get The Current Location Of The Tap
-        let currentTouchLocation = gesture.location(in: self.sceneView)
-        
-        //2. If We Hit An SCNNode Set It As The Current Node So We Can Interact With It
-        if let nodeHitTest = self.sceneView.hitTest(currentTouchLocation, options: nil).first?.node{
-            
-            currentNode = nodeHitTest
-            return
-        }
-        
-        //3. Do An ARHitTest For Features Points So We Can Place An SCNNode
-        if let hitTest = self.sceneView.hitTest(currentTouchLocation, types: .featurePoint).first {
-            
-            //4. Get The World Transform
-            let hitTestPosition = hitTest.worldTransform.columns.3
-            
-            //5. Add The TestNode At The Desired Position
-            createTextFromPosition(SCNVector3(hitTestPosition.x, hitTestPosition.y, hitTestPosition.z))
-            return
-            
-        }
-        currentNode?.removeFromParentNode()
-        
-    } */
-    
-    //-------------------
-    //MARK: Node Creation
-    //-------------------
-    
-    /// Creates An SCNNode With An SCNTextGeometry
-    ///
-    /// - Parameter position: SCNVector3
-    func createTextFromPosition(_ position: SCNVector3, _ name: String){
-        
-        let string = name
-        let text = SCNText(string: string, extrusionDepth: 0.1)
-        text.font = UIFont.systemFont(ofSize: 1)
-        text.flatness = 0.005
-        let textNode = SCNNode(geometry: text)
-        let fontScale: Float = 0.01
-        textNode.scale = SCNVector3(fontScale, fontScale, fontScale)
-        let (min, max) = (text.boundingBox.min, text.boundingBox.max)
-        let dx = min.x + 0.5 * (max.x - min.x)
-        let dy = min.y + 0.5 * (max.y - min.y)
-        let dz = min.z + 0.5 * (max.z - min.z)
-        textNode.pivot = SCNMatrix4MakeTranslation(dx, dy, dz)
-        let width = (max.x - min.x) * fontScale
-        let height = (max.y - min.y) * fontScale
-        let plane = SCNPlane(width: CGFloat(width), height: CGFloat(height))
-        let planeNode = SCNNode(geometry: plane)
-        planeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.green.withAlphaComponent(0.5)
-        planeNode.geometry?.firstMaterial?.isDoubleSided = true
-        planeNode.position = textNode.position
-        textNode.eulerAngles = planeNode.eulerAngles
-        planeNode.addChildNode(textNode)
-        sceneView.scene.rootNode.addChildNode(planeNode);
-        currentNode = planeNode;
-        
-    }
-    
-    
-    //-------------
-    //MARK: Scaling
-    //-------------
-    
-    /// Scales The Currently Selected Node
-    ///
-    /// - Parameter gesture: UIPinchGestureRecognizer
-    @objc func scaleCurrentNode(_ gesture: UIPinchGestureRecognizer) {
-        
-        if !isRotating, let selectedNode = currentNode{
-            
-            if gesture.state == .changed {
-                
-                let pinchScaleX: CGFloat = gesture.scale * CGFloat((selectedNode.scale.x))
-                let pinchScaleY: CGFloat = gesture.scale * CGFloat((selectedNode.scale.y))
-                let pinchScaleZ: CGFloat = gesture.scale * CGFloat((selectedNode.scale.z))
-                selectedNode.scale = SCNVector3Make(Float(pinchScaleX), Float(pinchScaleY), Float(pinchScaleZ))
-                gesture.scale = 1
-                
-            }
-            
-            if gesture.state == .ended {}
-        }
-        currentNode?.removeFromParentNode()
-    }
+ 
 }
