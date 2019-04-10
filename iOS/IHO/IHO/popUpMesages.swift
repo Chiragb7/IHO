67939a4600168e72b56893533136393708ebccc7
Vineesha Kasam <vineesha1416.cse@gmail.com>
2019-04-09 20:21:34 -0700

    US #100 Task #119: Adding the popup messages into a dictionary with their bone names as keys in a Swift file

---
+++ iOS/IHO/IHO/popUpMesages.swift
@@ -0,0 +1,33 @@
+//
+//  popUpMesages.swift
+//  IHO
+//
+//  Created by vkasam on 4/9/19.
+//  Copyright © 2019 vkasam. All rights reserved.
+//
+
+import Foundation
+
+public class popUpMessages {
+    
+    //Creating an empty dictionary
+    var popUpDict: [String: String] = [:]
+    
+    //adding key-value pairs into the dictionary with the bone name as the key and the popUp message as the value
+    init() {
+        
+        popUpDict.updateValue("Evidence for bipedality: The long bones of the foot in Australopithecus indicate that they had a human like longitudinal arc.", forKey: "rightFeet")
+        popUpDict.updateValue("Humans walk on two legs (bipedally) and have long legs and short arms. Chimpanzees walk on all fours and have longer arms than legs. Australopithecus are thought to have walked bipedally and had arms that are relatively shorter than chimpanzees but are relatively longer than humans.", forKey: "rightLeg")
+        popUpDict.updateValue("There is a controversial evidence for Australopithecus stone tool use. The oldest stone tool has been dated to 2.6 million years ago. It is possible that they were either using naturally broken stone as a tool or purposefully creating simple stone tools.", forKey: "leftFingers")
+        popUpDict.updateValue("Evidence for bipedality: The knee is angled inward like in humans, rather than the more vertical position found in chimpanzees. This difference in orientation moves the support the lower legs offer directly under the body's center of gravity", forKey: "leftLegBone")
+        popUpDict.updateValue("Evidence for bipedality: The pelvis is wide and bowl-shaped, as it is in humans.", forKey: "rightHip")
+        popUpDict.updateValue("Australopithecus afarensis lived in east Africa 3.9-2.9 million years ago in mosaic habitats of open woodland, grassland and shrubland", forKey: "rightHand")
+        popUpDict.updateValue("Evidence for bipedality: The lumbar portion of the spine curves inward as it does in humans to better support the weight of the upper body.", forKey: "spine")
+        popUpDict.updateValue("Lucy was discovered in exposed sediments in Hadar, Ethiopia. Additional fossils belonging to her species have also been found in othe sides in Ethiopia, Kenya and Tanzania.", forKey: "leftHand")
+        popUpDict.updateValue("The Australopithecus has a U shaped jaw in contrast with modern humans whose jaw is parabolic. The jaw muscles are also much larger in comparison to humans.", forKey: "lowerJaw")
+        popUpDict.updateValue("Estimates of brain size range from 380 to 550 cubic centimeters(cc). This is similar in size to that of chimpanzees whse average 395cc and much smaller than modern humans whose average is 1475cc.", forKey: "topSkull")
+        popUpDict.updateValue("Australopithecus afarensis had a strongly protruding face similar to that of chimpanzees while humans are categorized by short faces", forKey: "skull")
+        
+    }
+    
+}
