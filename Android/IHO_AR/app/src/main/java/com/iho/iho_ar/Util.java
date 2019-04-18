79d5ad1938b790508ba076451079a26cfcdb0b5a
Chiragb7 <cbumb@asu.edu>
2019-04-17 21:41:25 -0700

    Task #112

--- Android/IHO_AR/app/src/main/java/com/iho/iho_ar/Util.java
+++ Android/IHO_AR/app/src/main/java/com/iho/iho_ar/Util.java
@@ -1,22 +1,46 @@
 package com.iho.iho_ar;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 
-public class Util {
+//public class Util {
+//
+//    public static void createPopup(Context context, String title, String message){
+//        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+//                .setTitle(title)
+//                .setMessage(message)
+//                .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                    }
+//                });
+//
+//        builder.show();
+//    }
+//}
 
-    public static void createPopup(Context context, String title, String message){
-        AlertDialog.Builder builder = new AlertDialog.Builder(context)
-                .setTitle(title)
-                .setMessage(message)
-                .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
+public class FireMissilesDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.dialog_fire_missiles)
+                .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
                     }
                 });
-
-        builder.show();
+        // Create the AlertDialog object and return it
+        return builder.create();
     }
 }
